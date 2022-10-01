6e5dea4974aea16c9d9beb21d068e0dd1d2b7901
lucint <100629618+lucint@users.noreply.github.com>
2022-10-01 10:52:47 +0700

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,15 @@
+#INSTALL XMRIG
+
+apt update 
+apt install wget
+apt install proot
+apt install git
+apt-get install git libuv-dev clang cmake nano
+mkdir xmrig
+cd xmrig
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake .. -DWITH_HTTPD=OFF -DWITH_TLS=OFF
+make
