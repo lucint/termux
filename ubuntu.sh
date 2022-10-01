ab72509461ab55382cc4ed7d8f242482b6bfcc0a
lucint <100629618+lucint@users.noreply.github.com>
2022-10-01 11:56:40 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,8 +4,6 @@ apt update
 apt install wget
 apt install proot
 apt install git
-mkdir ubuntu18.04
-cd ubuntu18.04
 wget https://github.com/lucint/termux/raw/main/ubuntu-termux.tar.gz
 tar -xf ubuntu-termux.tar.gz
 cd ubuntu-termux
