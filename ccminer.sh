f61992a295cbc5136e10afc67c814dc94fffe4f8
lucint <100629618+lucint@users.noreply.github.com>
2022-10-01 13:06:58 +0700

    Update ccminer.sh

--- ccminer.sh
+++ ccminer.sh
@@ -5,8 +5,6 @@ apt install wget
 apt install proot
 apt-get install git
 apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-mkdir ccminer
-cd ccminer
 git clone --single-branch -b ARM https://github.com/monkins1010/ccminer
 cd ccminer
 chmod +x build.sh
