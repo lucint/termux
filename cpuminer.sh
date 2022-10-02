21524be1d7f7a3daee9a3d83f647b145d0d9301c
lucint <100629618+lucint@users.noreply.github.com>
2022-10-02 07:44:32 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -6,5 +6,5 @@ apt install proot
 apt install git
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
 git clone https://github.com/wong-fi-hung/termux-miner.git
-cd cpuminer-multi
+cd termux-miner
 ./build-android.sh
